7b21935ea0dd7fc7a1c5646df60eb1e7037f104e
Aaron Addleman <aaddleman@frbsf.org>
2012-03-22 21:44:04 -0700

    trying to fix missing comma problem

--- app/assets/javascripts/mercury.js
+++ app/assets/javascripts/mercury.js
@@ -478,7 +478,7 @@ window.Mercury = {
 	  var link = $('#mercury_iframe').contents().find('#edit_link');
 	  Mercury.saveURL = link.data('save-url');
 	  link.hide();
-	},
+	});
 	
 	$(window).bind('mercury:saved', function() {
 	  window.location = window.location.href.replace(/\/editor\//i, '/');
@@ -488,6 +488,3 @@ window.Mercury = {
   },
 
 });
-
-
-};
