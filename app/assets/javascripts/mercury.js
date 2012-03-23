f9b74608614752fffbf066675f1748f9d48cf224
Aaron Addleman <aaddleman@frbsf.org>
2012-03-22 21:50:03 -0700

    tried new approach of removing extra brackets

--- app/assets/javascripts/mercury.js
+++ app/assets/javascripts/mercury.js
@@ -478,13 +478,13 @@ window.Mercury = {
 	  var link = $('#mercury_iframe').contents().find('#edit_link');
 	  Mercury.saveURL = link.data('save-url');
 	  link.hide();
-	});
+	},
 	
 	$(window).bind('mercury:saved', function() {
 	  window.location = window.location.href.replace(/\/editor\//i, '/');
-	});
+	},
 	
 
   },
 
-});
+},
