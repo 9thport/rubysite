d64a30531c4528b3e0bb64139dbc97713d82e513
Aaron Addleman <aaddleman@frbsf.org>
2012-03-22 21:38:30 -0700

    added comma and mustache

--- app/assets/javascripts/mercury.js
+++ app/assets/javascripts/mercury.js
@@ -474,16 +474,20 @@ window.Mercury = {
   // place to add or change functionality.
   onload: function() {
     //Mercury.PageEditor.prototype.iframeSrc = function(url) { return '/testing'; }
+	  $(window).bind('mercury:ready', function() {
+	  var link = $('#mercury_iframe').contents().find('#edit_link');
+	  Mercury.saveURL = link.data('save-url');
+	  link.hide();
+	},
+	
+	$(window).bind('mercury:saved', function() {
+	  window.location = window.location.href.replace(/\/editor\//i, '/');
+	});
+	
+
   },
 
-  $(window).bind('mercury:ready', function() {
-  var link = $('#mercury_iframe').contents().find('#edit_link');
-  Mercury.saveURL = link.data('save-url');
-  link.hide();
 });
 
-$(window).bind('mercury:saved', function() {
-  window.location = window.location.href.replace(/\/editor\//i, '/');
-});
 
 };
