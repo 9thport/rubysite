380864dbd5853c1ece8c7213a6f12e84e683c985
Aaron Addleman <aaddleman@frbsf.org>
2012-03-26 08:09:52 -0700

    added red cloth and syntax highlighting with albino and nokogiri

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.2'
+gem 'RedCloth'
+gem 'albino'
+gem 'nokogiri'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def markdown(text)
+    syntax_highlighter(RedCloth.new(text).to_html)
+    # RedCloth.new(text).to_html
+    # syntax_highlighter(Redcarpet.new(text, *options).to_html).html_safe
+  end
+  
+  def syntax_highlighter(html)
+    doc = Nokogiri::HTML(html)
+    doc.search("//pre[@lang]").each do |pre|
+      pre.replace Albino.colorize(pre.text.rstrip, pre[:lang])
+    end
+    doc.to_s
+  end
 end
